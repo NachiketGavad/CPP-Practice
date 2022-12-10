cc0c9cfcb587ed0b54f8f3d9d50e6df9e0f48e50
NachiketGavad <nachiketgavad@gmail.com>
2022-12-10 15:32:28 +0530

    update

--- DSA/Leetcode answers/110_balanced-btree.cpp
+++ DSA/Leetcode answers/110_balanced-btree.cpp
@@ -37,4 +37,24 @@ public:
         }
         return false;
     }
+    
+    pair<bool,int> isBalancedfast(TreeNode* root) {
+        if(root==NULL){
+            pair<bool,int> p=make_pair(true,0);
+            return p;
+        }
+
+        pair<bool,int> left=isBalancedfast(root->left);
+        pair<bool,int> right=isBalancedfast(root->right);
+        bool valu=abs(left.second-right.second)<=1;
+
+        pair<bool,int> ans;
+        ans.second=max(left.second,right.second)+1;
+        ans.first=left.first && right.first && valu;
+        return ans;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return isBalancedfast(root).first;
+    }
 };
